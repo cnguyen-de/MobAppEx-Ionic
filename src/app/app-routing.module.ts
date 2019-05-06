f0b93938aab97323c2fe9a9ba9c67b51c44a376f
Chi Nguyen <c-nguyen-prog@users.noreply.github.com>
2019-05-06 19:08:17 +0200

    addded welcome slides, homepage, added bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './homepage/homepage.module#HomepagePageModule' },
+  { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'slides', loadChildren: './slides/slides.module#SlidesPageModule' }
+
+
 ];
 @NgModule({
   imports: [
