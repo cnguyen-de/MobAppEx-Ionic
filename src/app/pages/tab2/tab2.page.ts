f2cf522f8aed0a46564d38c8036516cfc8d4ad49
Dave Zzz <esteepee@live.com>
2019-05-28 15:17:05 +0200

    distance calc test

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -25,9 +25,9 @@ export class Tab2Page implements OnInit {
   capsules = [];
 
   slidesConfig = {
-    spaceBetween: 1,
+    spaceBetween: 10,
     centeredSlides: true,
-    slidesPerView: 1.6
+    slidesPerView: 1.5
   };
 
   ngOnInit() {
@@ -86,6 +86,26 @@ export class Tab2Page implements OnInit {
     this.slider.getActiveIndex().then(data => {
       this.capsules[data].isOpen = true;
     });
+  }
+
+  // https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+  getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    //return d;
+    return Math.round(d * 1000);
+  }
+  
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
   }
 
   // results: Observable<any>;
