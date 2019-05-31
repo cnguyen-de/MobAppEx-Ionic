c43398be611b6cd258e42c5f1c4ccadd75ec0a41
Chi Nguyen <c-nguyen-prog@users.noreply.github.com>
2019-05-31 23:07:00 +0200

    added Paypal Integration #11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { TokenInterceptor} from './_services/token-interceptor';
 import {ErrorInterceptor} from './_services/error-interceptor';
 
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {PayPal} from '@ionic-native/paypal/ngx';
 
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -51,9 +52,10 @@ export function HttpLoaderFactory(http: HttpClient) {
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    PayPal,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy, multi: false},
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
-    Geolocation,
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
 
   ],
--- src/app/modals/checkout-modal/checkout-modal.page.ts
+++ src/app/modals/checkout-modal/checkout-modal.page.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Storage} from '@ionic/storage';
 import {ApiService} from '../../_services/api/api.service';
 import {ModalController, NavParams} from '@ionic/angular';
+import { PayPal, PayPalPayment, PayPalConfiguration } from '@ionic-native/paypal/ngx';
 
 @Component({
   selector: 'app-checkout-modal',
@@ -10,17 +11,39 @@ import {ModalController, NavParams} from '@ionic/angular';
   styleUrls: ['./checkout-modal.page.scss'],
 })
 export class CheckoutModalPage implements OnInit {
-  @Input() value: string;
+  @Input() paymentAmount: string;
+  currency: string = 'EUR';
+  currencyIcon: string = '€';
+
   constructor(private http : HttpClient,
               public storage : Storage, private apiService: ApiService,
-              private navParams: NavParams, private modalController: ModalController) {
-    this.value = this.navParams.get('value');
+              private navParams: NavParams, private modalController: ModalController,
+              private payPal: PayPal) {
+    this.paymentAmount = this.navParams.get('value');
   }
 
   ngOnInit() {
   }
 
+  payWithPayPal() {
+    this.payPal.init({
+      PayPalEnvironmentProduction: 'Ae0rLWsukh4CEYjyOx2a1jO_27XN9ZUtvYcN1GTjfkPc2Q5e1rQlNRv-65ikwfgIct-5MoEx8fQJ9ssT',
+      PayPalEnvironmentSandbox: 'AcuTHx8v5tlyGqbRiNVviT1-yc5MMciXSg1E1srIgObOU8Y5Q6sGyAw2taBmqBSgLDe5akHCe8aqmaO5',
+    }).then(() => {
+      this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
+
+      })).then(() => {
+        let payment = new PayPalPayment(this.paymentAmount, this.currency, 'Description', 'sale');
+        this.payPal.renderSinglePaymentUI(payment).then((res) => {
+          console.log(res)
+        }, (err) => {
+          console.log("ERROR rendering")
+        })
+      }, (err) => console.log("ERROR configuration"))
+    }, (err) => console.log("ERROR initializing"))
+  }
+
   dismiss() {
-    this.modalController.dismiss(this.value);
+    this.modalController.dismiss(this.paymentAmount);
   }
 }
