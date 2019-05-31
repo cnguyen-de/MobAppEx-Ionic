f5a9a23b5eb5a8ffe44a872b2372eef35ce4b38f
Chi Nguyen <c-nguyen-prog@users.noreply.github.com>
2019-05-31 17:45:26 +0200

    QOL update for login page, password is cleared after wrong input, hide header

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -59,6 +59,7 @@ export class LoginPage implements OnInit {
             },
             error => {
               this.loginPressed = !this.loginPressed;
+              this.loginForm.setValue({username: this.loginForm.value.username, password: ''});
               console.log(error);
               if (error.status === 0 || error.status === 504) {
                 this.toast("Unable to communicate with server")
