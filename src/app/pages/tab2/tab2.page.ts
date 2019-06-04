f5c94b8cd4f967fda129342980fe4108a88ea6a7
Dave Zzz <esteepee@live.com>
2019-06-04 08:47:22 +0200

    timeslot selection animation

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,7 +1,5 @@
-import { SlidesPage } from './../slides/slides.page';
 import { Component, ViewChild, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
-import { IonSlides, Platform} from "@ionic/angular";
+import { IonSlides, IonSegment, IonContent, Platform} from "@ionic/angular";
 import { LocationService } from '../../_services/location.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { ApiService } from '../../_services/api/api.service';
@@ -18,7 +16,8 @@ import { ApiService } from '../../_services/api/api.service';
        })),
        state('top',style({
           //transform : 'translateY(-490px)',
-          width: 'calc(100% - 16px)',
+          width: 'calc(100% - 20px)',
+          height: '120px',
           position: 'absolute',
           top: 0,
           left: 0
@@ -40,6 +39,10 @@ import { ApiService } from '../../_services/api/api.service';
 })
 export class Tab2Page implements OnInit {
   @ViewChild("slider") slider: IonSlides;
+  @ViewChild('segment') segment: IonSegment;
+  @ViewChild('content') content: IonContent;
+  @ViewChild('slides') slides:IonSlides;
+
 
   constructor(private locationService: LocationService,
     private apiService: ApiService,
@@ -54,6 +57,7 @@ export class Tab2Page implements OnInit {
   lngMapCenter: number = 8.69238764;
   personIcon: string = '../../../assets/images/icons/LocationPerson.svg';
   capsuleIcon: string = '../../../assets/images/icons/SnoozeMarker.svg';
+  spinBtnPositionPressed = false;
   
 
   //Slider Configs
@@ -61,6 +65,12 @@ export class Tab2Page implements OnInit {
     spaceBetween: 10,
     centeredSlides: true,
     slidesPerView: 1.5
+  };
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    loop: true
   };
 
   //Animations
@@ -71,6 +81,12 @@ export class Tab2Page implements OnInit {
 
   //Capsules
   capsules: any;
+
+  //Timeslots
+  timeslots = [];
+  segmentWidth: number = 100;
+
+  test = false;
 
   ngOnInit() {
     // Demo Data
@@ -118,6 +134,92 @@ export class Tab2Page implements OnInit {
         this.animateTSS_Click();
       }
     });
+
+    
+
+    this.timeslots = [
+      {
+        content: '9:00 - 9:20', state: 'true'
+      },
+      {
+        content: '9:20 - 9:40', state: 'selected'
+      },
+      {
+        content: '9:40 - 10:00', state: 'false'
+      },
+      {
+        content: '10:00 - 10:20', state: 'blocked'
+      },
+      {
+        content: '10:20 - 10:40', state: 'inprogress'
+      },
+      {
+        content: '10:40 - 11:00', state: 'booked'
+      },
+      {
+        content: '11:00 - 11:20', state: 'booked'
+      },
+      {
+        content: '11:20 - 11:40', state: 'blocked'
+      },
+      {
+        content: '11:40 - 12:00', state: 'false'
+      },
+      {
+        content: '12:00 - 12:20', state: 'false'
+      },
+      {
+        content: '12:20 - 12:40', state: 'false'
+      },
+      {
+        content: '12:40 - 13:00', state: 'false'
+      },
+      {
+        content: '13:00 - 13:20', state: 'false'
+      },
+      {
+        content: '13:20 - 13:40', state: 'false'
+      },
+      {
+        content: '13:40 - 14:00', state: 'false'
+      },
+      {
+        content: '14:00 - 14:20', state: 'false'
+      },
+      {
+        content: '14:20 - 14:40', state: 'false'
+      },
+      {
+        content: '14:40 - 15:00', state: 'false'
+      },
+      {
+        content: '15:00 - 15:20', state: 'false'
+      },
+      {
+        content: '15:20 - 15:40', state: 'false'
+      },
+      {
+        content: '15:40 - 16:00', state: 'false'
+      },
+      {
+        content: '16:00 - 16:20', state: 'false'
+      },
+      {
+        content: '16:20 - 16:40', state: 'false'
+      },
+      {
+        content: '16:40 - 17:00', state: 'false'
+      },
+      {
+        content: '17:00 - 17:20', state: 'false'
+      },
+      {
+        content: '17:20 - 17:40', state: 'false'
+      },
+      {
+        content: '17:40 - 18:00', state: 'false'
+      },
+    ]
   }
 
   
@@ -163,6 +265,17 @@ export class Tab2Page implements OnInit {
       elem.setAttribute("style", "visibility: hidden");
       let elem2 = await document.getElementById("slider");
       elem2.setAttribute("style", "visibility: visible");
+    } else {
+      //this.test = true;
+      
+      setTimeout(() => {
+        let elem2 = document.getElementById("root");
+        elem2.setAttribute("style", "visibility: visible");
+
+        let elem: HTMLElement = document.getElementById('segmt');
+        elem.setAttribute("style", "min-width: " + (this.segmentWidth * 10).toString() + "px" );
+      }, 100);
+      
     }
   }
 
@@ -171,11 +284,13 @@ export class Tab2Page implements OnInit {
     if(item) {
       this.capName = item.Name;
     }
+    let elemo = await document.getElementById("root");
+    elemo.setAttribute("style", "visibility: hidden");
 
 
     let elem = await document.getElementById("cardTSS_top");
     let elem3 = await document.getElementById("slideCard");
-    
+    console.log('cardTSS_top-width: ' + elem3.offsetWidth)
     elem.setAttribute("style", "visibility: visible; width: " + elem3.offsetWidth + "px");
     let elem2 = await document.getElementById("slider");
     elem2.setAttribute("style", "visibility: hidden");
@@ -189,6 +304,104 @@ export class Tab2Page implements OnInit {
       this.latMapCenter = data.coords.latitude;
       this.lngMapCenter = data.coords.longitude;
     });
+  }
+
+
+
+
+
+
+
+  doRefresh(event) {
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+   
+  }
+
+  onButtonClick() {
+    this.segment.value = "2";
+    this.content.scrollToPoint(2*this.segmentWidth,0,200);
+  }
+
+  async onIonSlideDidChange(event?) {
+    // let indexp = await this.slides.getPreviousIndex();
+    // let indexc = await this.slides.getActiveIndex();
+    // console.log(indexp + ' : ' + indexc)
+    this.slides.length().then(data => {
+      console.log(data);
+    });
+    let elem = document.getElementsByClassName("even");
+    elem[0].setAttribute("style", "color:black; border: 0px solid blue;");
+    elem[1].setAttribute("style", "color:black; border: 0px solid blue;");
+
+      let elem2 = document.getElementsByClassName("odd");
+      elem2[0].setAttribute("style", "color:black; border: 0px solid blue;");
+      elem2[1].setAttribute("style", "color:black; border: 0px solid blue;");
+
+    
+
+  //   this.clickSegment(index);
+    // this.timeslots = [];
+    // setTimeout(() => {
+    //   this.timeslots = this.timeslots2;
+
+    // }, 1000);
+    
+  }
+
+
+  async onIonSlidePrevStart() {
+    //console.log('prev start')
+    let index = await +this.segment.value;
+    if(index === 0) {
+      index = 10;
+    }
+    this.segment.value = (index - 1).toString();
+    this.content.scrollToPoint((index-1)*this.segmentWidth,0,200);
+  }
+
+  onIonSlidePrevEnd() {
+    //console.log('prev ended')
+  }
+
+  async onIonSlideNextStart() {
+    //console.log('next start')
+    let index = await +this.segment.value;
+    if(index === 9) {
+      index = -1;
+    }
+    this.segment.value = (index + 1).toString();
+    this.content.scrollToPoint((index+1)*this.segmentWidth,0,200);
+  }
+
+  onIonSlideNextEnd() {
+    //console.log('next ended')
+  }
+
+  async onIonSlideTouchStart() {
+    //let indexp = await this.slides.getPreviousIndex();
+    let indexc = await this.slides.getActiveIndex();
+    
+    //console.log(indexp + ' : ' + indexc)
+
+    if(indexc === 0 || indexc ===2) {
+      let elem = document.getElementsByClassName("odd");
+      elem[0].setAttribute("style", "color:red; border: 0px solid blue;");
+      elem[1].setAttribute("style", "color:green; border: 0px solid blue;");
+      let elem2 = document.getElementsByClassName("even");
+      elem2[0].setAttribute("style", "color:black; border: 0px solid blue;");
+      elem2[1].setAttribute("style", "color:black; border: 0px solid blue;");
+    } else {
+      let elem = document.getElementsByClassName("even");
+      elem[0].setAttribute("style", "color:red; border: 0px solid blue;");
+      elem[1].setAttribute("style", "color:green; border: 0px solid blue;");
+      let elem2 = document.getElementsByClassName("odd");
+      elem2[0].setAttribute("style", "color:black; border: 0px solid blue;");
+      elem2[1].setAttribute("style", "color:black; border: 0px solid blue;");
+    }
+    
   }
 
   // results: Observable<any>;
